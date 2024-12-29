aa7943a2655c021ccbca34ac8630b527afc0c9db
Albu Tudor <tudor.albu@student.usv.ro>
2024-12-29 17:00:05 +0200

    Storing Urls

--- Api/src/UrlShortener.Api/Extensions/ServiceCollectionExtensions.cs
+++ Api/src/UrlShortener.Api/Extensions/ServiceCollectionExtensions.cs
@@ -16,19 +16,8 @@ namespace UrlShortener.Api.Extensions
                 return tokenProvider;
             });
             services.AddScoped<ShortUrlGenerator>();
-            services.AddSingleton<IUrlDataStore, InMemoryUrlDataStore>();
 
             return services;
-        }
-    }
-
-    public class InMemoryUrlDataStore : Dictionary<string, ShortenedUrl>, IUrlDataStore
-    {
-        public Task AddAsync(ShortenedUrl shortened, CancellationToken cancellationToken)
-        {
-            Add(shortened.ShortUrl, shortened);
-
-            return Task.CompletedTask;
         }
     }
 }
