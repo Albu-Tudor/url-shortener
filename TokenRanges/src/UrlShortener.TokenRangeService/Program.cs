acbdfd40194c88224d82bdefffa70a8376f31546
Albu Tudor <tudor.albu@student.usv.ro>
2024-12-30 15:26:44 +0200

    assign token ranges

--- TokenRanges/src/UrlShortener.TokenRangeService/Program.cs
+++ TokenRanges/src/UrlShortener.TokenRangeService/Program.cs
@@ -1,21 +1,21 @@
+using UrlShortener.TokenRangeService;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton(
+    new TokenRangeManager(builder.Configuration["Postgres:ConnectionString"]!));
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 
 app.MapGet("/", () => "TokenRanges Service");
+app.MapPost("assign", 
+    async (AssignTokenRangeRequest request, TokenRangeManager manager) =>
+    {
+        var range = await manager.AssignRangeAsync(request.Key);
+
+        return range;
+    });
 
 app.Run();
